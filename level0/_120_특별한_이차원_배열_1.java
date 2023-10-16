ffd24f024b50e30f21618348ab1f92d783753ca6
CMN <chaalsgmll@gmail.com>
2023-10-17 07:53:35 +0900

    Update: 20231017

--- level0/_120_특별한_이차원_배열_1.java
+++ level0/_120_특별한_이차원_배열_1.java
@@ -3,9 +3,6 @@ package level0;
 public class _120_특별한_이차원_배열_1 {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        for (int i=0; i<n; i++) {
-            answer[i][i] = 1;
-        }
         return answer;
     }
 }

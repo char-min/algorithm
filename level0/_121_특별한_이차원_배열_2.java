36ba53e813a039fc2223b9f019a4871e126d8a1f
CMN <chaalsgmll@gmail.com>
2023-10-18 07:45:28 +0900

    Update: 2021018 solving

---
+++ level0/_121_특별한_이차원_배열_2.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class _121_특별한_이차원_배열_2 {
+    public int solution(int[][] arr) {
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr[0].length; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}

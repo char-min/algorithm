f69a97ea28d88dcec7cf64b5022de48a8ca273a4
CMN <chaalsgmll@gmail.com>
2023-11-02 07:50:39 +0900

    Update: 20231102 solving

---
+++ level0/_163_평행.java
@@ -0,0 +1,13 @@
+package level0;
+
+public class _163_평행 {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        boolean isParallel = isParallel(dots);
+        return isParallel ? 1 : 0;
+    }
+
+    private boolean isParallel(int[][] dots) {
+        return false;
+    }
+}

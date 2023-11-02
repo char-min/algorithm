aa2de4fb100b0f0b4c0888276f7da4f4f91621a5
CMN <chaalsgmll@gmail.com>
2023-11-03 07:38:21 +0900

    Update: 20231103 solving

--- level0/_163_평행.java
+++ level0/_163_평행.java
@@ -2,12 +2,25 @@ package level0;
 
 public class _163_평행 {
     public int solution(int[][] dots) {
-        int answer = 0;
         boolean isParallel = isParallel(dots);
+        if (!isParallel) {
+            swap(dots, 1, 2);
+            isParallel = isParallel(dots);
+        }
         return isParallel ? 1 : 0;
     }
 
+    private void swap(int[][] dots, int i, int j) {
+        int[] tmp = dots[i];
+        dots[i] = dots[j];
+        dots[j] = tmp;
+    }
+
     private boolean isParallel(int[][] dots) {
-        return false;
+        int dx1 = dots[1][0] - dots[0][0];
+        int dx2 = dots[3][0] - dots[2][0];
+        int dy1 = dots[1][1] - dots[0][1];
+        int dy2 = dots[3][1] - dots[2][1];
+        return ((float)dx1) / dx2 == ((float)dy1) / dy2;
     }
 }
